aa9208883ec8274302aaaf94a8363635bebf52b2
Shivam Goyal <shivamgoyal1899@gmail.com>
2019-06-05 14:41:42 +0530

    Updates scripts

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -1,27 +1,10 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 'use strict';
 
 let deferredInstallPrompt = null;
 const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
-// CODELAB: Add event listener for beforeinstallprompt event
+// Add event listener for beforeinstallprompt event
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 /**
@@ -31,7 +14,7 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
  * @param {Event} evt
  */
 function saveBeforeInstallPromptEvent(evt) {
-  // CODELAB: Add code to save event & show the install button.
+  // Add code to save event & show the install button.
   deferredInstallPrompt = evt;
   installButton.removeAttribute('hidden');
 }
@@ -43,13 +26,13 @@ function saveBeforeInstallPromptEvent(evt) {
  * @param {Event} evt
  */
 function installPWA(evt) {
-  // CODELAB: Add code show install prompt & hide the install button.
+  // Add code show install prompt & hide the install button.
   deferredInstallPrompt.prompt();
   // Hide the install button, it can't be called twice.
   evt.srcElement.setAttribute('hidden', true);
 }
 
-// CODELAB: Add event listener for appinstalled event
+// Add event listener for appinstalled event
 window.addEventListener('appinstalled', logAppInstalled);
 /**
  * Event handler for appinstalled event.
@@ -58,6 +41,6 @@ window.addEventListener('appinstalled', logAppInstalled);
  * @param {Event} evt
  */
 function logAppInstalled(evt) {
-  // CODELAB: Add code to log the event
+  // Add code to log the event
   console.log('Weather App was installed.', evt);
 }
